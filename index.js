c2069f382c3f189dccd5af1b2f71eb91e9a85e4c
spring1826 <dongwon1826@gmail.com>
2020-06-09 23:50:27 +0900

    MongoDB&mongoose_install

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true })); //client가 요청한 정보를 서버로부터 받는다
+app.use(bodyParser.json()); //json타입을 받는다
 
 const mongoose = require("mongoose");
 mongoose
@@ -17,5 +22,14 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  }); //mongoDB에서 오는 메소드
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
